5c36ecbba4808f1fb141336406cee328f75a5558
juanrar <juanchilorenzo@gmail.com>
2026-09-08 13:40:10 -0300

    fix: update show title to reflect event name

--- lib/show.ts
+++ lib/show.ts
@@ -6,7 +6,7 @@ export interface Show {
 }
 
 export const SHOW: Show = {
-  title: 'Función de prueba',
+  title: 'Muestra de fin de año',
   venue: 'Teatro del Globo',
   address: 'Marcelo T. de Alvear 1155, CABA',
   startsAt: '2026-09-20T21:00:00-03:00',
